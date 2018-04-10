40ee54b0325cb8075b0a91d548055fd3719deb0f
slledru <slledru@msn.com>
2018-04-10 09:33:46 -0600

    corrected db name

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,19 @@
 module.exports = {
   development: {
     client: 'pg',
+    directory: 'data',
     connection: 'postgres://localhost/g81_reads_dev'
   },
 
   test: {
     client: 'pg',
+    directory: 'data',
     connection: 'postgres://localhost/g81_reads_test'
   },
 
   production: {
     client: 'pg',
+    directory: 'data',
     connection: process.env.DATABASE_URL
   }
 }
