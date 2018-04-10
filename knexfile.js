d760587473c1f6804839d5fe857d34083909430a
slledru <slledru@msn.com>
2018-04-10 09:59:46 -0600

    added directories for migrate and seed

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,37 @@
 'use strict'
+const path = require('path')
 
 module.exports = {
   development: {
     client: 'pg',
-    directory: 'data',
+    migrations: {
+      directory: path.join(__dirname, 'data', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'data', 'seeds')
+    },
     connection: 'postgres://localhost/g81_reads_dev'
   },
 
   test: {
     client: 'pg',
-    directory: 'data',
+    migrations: {
+      directory: path.join(__dirname, 'data', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'data', 'seeds')
+    },
     connection: 'postgres://localhost/g81_reads_test'
   },
 
   production: {
     client: 'pg',
-    directory: 'data',
+    migrations: {
+      directory: path.join(__dirname, 'data', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'data', 'seeds')
+    },
     connection: process.env.DATABASE_URL
   }
 }
