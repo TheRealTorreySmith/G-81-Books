56afad4a55cbe8d4d1d474d80998413bce497939
Keila Hoover <keila.hoover@gmail.com>
2018-04-11 08:30:41 -0600

    Remove humps from routes/authors.js

--- routes/authors.js
+++ routes/authors.js
@@ -1,46 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
-const { camelizeKeys, decamelizeKeys } = require('humps')
 const boom = require('boom')
 
 const allAuthors = (req, res, next) => {
   res.render('authors', { title: 'Authors Home Page' })
 }
-//
-// 'use strict';
-//
-// const express = require('express');
-//
-// // eslint-disable-next-line new-cap
-// const router = express.Router();
-// var knex = require('../knex')
-// var humps = require('humps')
-//
-// router.get('/', (req, res, next) => {
-//   knex('books')
-//     .select('*')
-//     .then((rows) => rows.sort((title1, title2) => title1.title.toUpperCase() > title2.title.toUpperCase()))
-//     .then((rows) => rows.map((data) => humps.camelizeKeys(data)))
-//     .then((rows) => {
-//       res.json(rows)
-//     })
-//     .catch((err) => console.log(err))
-// })
-//
-// router.get('/:id', (req, res, next) => {
-//   const { id } = req.params
-//   knex('books')
-//     .select('*')
-//     .where('id', id)
-//     .then(rows => {
-//       if (rows.length > 0) {
-//         res.json(humps.camelizeKeys(rows[0]))
-//       } else {
-//         res.sendStatus(404)
-//       }
-//     })
-// })
+
 
 const newAuthorPage = (req, res, next) => {
   res.render('authors', { title: 'New Authors Page' })
