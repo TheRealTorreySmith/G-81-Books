4890d788ad1b6a32aee2ee701aa815275b57df04
Keila Hoover <keila.hoover@gmail.com>
2018-04-10 20:06:45 -0600

    Add public/javascript/all-authors file path

--- public/javascript/all-authors.js
+++ public/javascript/all-authors.js
@@ -1,3 +1,19 @@
-$('main__section').on('click', function () {
 
-})
+
+$('main__section').append(`
+  <img class="portrait" alt="author-image">
+  <article class="top-section">
+    <h2>Author Name</h2>
+    <button type="submit" class="main__btn">Edit</button>
+    <button type="submit" class="main__btn">Remove</button>
+  </article>
+  <article class="bottom-section">
+    <p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+    De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia?</p>
+    <ul class="main__ul">
+      <li>Book One</li>
+      <li>Book Two</li>
+      <li>Book Three</li>
+    </ul>
+  </article>
+  `)
