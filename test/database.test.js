4f9b69aacee2a8235c757525e52f094452f71413
slledru <slledru@msn.com>
2018-04-11 12:18:57 -0600

    test case for author

--- test/database.test.js
+++ test/database.test.js
@@ -3,33 +3,73 @@ const path = require('path')
 const expect = require('chai').expect
 const AuthorService = require('../services/author_service')
 const BookService = require('../services/book_service')
+const knex = require('../knex')
+
+beforeEach((done) => {
+  knex.migrate.rollback()
+    .then(() => {
+      return knex.migrate.latest()
+    })
+    .then(() => {
+      return knex.seed.run()
+    })
+    .finally(() => {
+      done()
+    })
+})
+
+afterEach((done) => {
+  knex.migrate.rollback()
+    .finally(() => {
+      done()
+    })
+})
+
+after(() => {
+  knex.destroy()
+})
 
 describe('database check', () => {
-  describe('authors table check', () => {
-    xit('should build a authorService instance', () => {
+  describe('authors table', () => {
+    it('should build a authorService instance', (done) => {
       const authorService = new AuthorService()
       expect(authorService).to.be.ok
       expect(authorService).to.be.an.instanceof(AuthorService)
       authorService.getAuthors()
         .then((rows) => {
-          //console.log(rows);
-          done()
+          expect(rows.length).to.equal(8)
+          return done()
         })
         .catch((err) => {
+          return done()
         })
     })
-    it('delete author', (done) => {
+    it('delete existing author', (done) => {
       const authorService = new AuthorService()
       authorService.deleteAuthorById(1)
         .then((row) => {
-          console.log(row)
           expect(row.id).to.equal(1)
+          return done()
         })
-      done()
+        .catch((err) => {
+          return done()
+        })
+    })
+    it('delete non-existing author', (done) => {
+      const authorService = new AuthorService()
+      authorService.deleteAuthorById(100)
+        .then((row) => {
+          expect(true).to.equal(false)
+          return done()
+        })
+        .catch((err) => {
+          expect(true).to.equal(true)
+          return done()
+        })
     })
   })
 
-  xdescribe('books table check', () => {
+  describe('books table check', () => {
     it('should build a bookService instance', () => {
       const bookService = new BookService()
 
